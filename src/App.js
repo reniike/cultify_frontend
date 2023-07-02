3c7fc727bc29827f78b03142b306dc3351952723
Sgreen <Sgreensneh@gmail.com>
2023-07-02 07:58:07 +0100

    registration_for_consumer_transporter_farmer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import FarmerRistration from "./pages/dashboard/components/FarmerRegistration";
+import Header from "./pages/dashboard/components/Header";
+import TransporterRistration from "./pages/dashboard/components/TransporterRegistration";
+import Register from "./pages/dashboard/components/Register";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/farmer" element={<FarmerRistration />} />
+        <Route path="/transporter" element={<TransporterRistration />} />
+      </Routes>
     </div>
   );
 }
