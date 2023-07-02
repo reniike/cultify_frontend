7dacf94165e55e8bb37704c6a27387bacfe73ca4
Moyinoluwa Michael <ogunsmoyin.m@gmail.com>
2023-07-02 13:51:51 +0100

    User registration updates

---
+++ src/pages/dashboard/components/consumerDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ConsumerDashboard(){
+
+    return(
+        <React.Fragment>
+            <div>
+                Hello
+            </div>
+        </React.Fragment>
+    )
+}
