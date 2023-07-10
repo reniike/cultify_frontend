8582fc28a7582c30c1a28345867fa9561c8d2299
Sgreen <Sgreensneh@gmail.com>
2023-07-10 15:38:46 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Login from "./pages/login/components/Login";
-import Otp from "./pages/registration/components/otp";
-import RegistrationRouter from "./pages/registration/components/registrationRouter";
-import ConsumerRegistration from "./pages/registration/components/consumerRegistration";
-import FarmerRegistration from "./pages/registration/components/farmerRegistration";
-import TransporterRegistration from "./pages/registration/components/transporterRegistration";
-import ConsumerDashboard from "./pages/dashboard/components/consumerDashboard";
+// import './App.css';
+import Login from "./pages/login/components/login";
+import Otp from "./pages/dashboard/components/otp";
+import InvestorRegistrationPage from "./pages/dashboard/components/investorRegistrationPage";
+import InvestorDashboard from "./pages/dashboard/components/investorDashboard";
+import ErrorPage from "./pages/dashboard/components/errorPage";
+import FarmProjectDetails from "./pages/dashboard/components/farmProjectDetails";
+import FarmProjects from "./pages/dashboard/components/farmProjects";
+import SuperAdminDashboardOptions from "./pages/dashboard/components/superAdminDashboardOptions";
+import Admins from "./pages/dashboard/components/admins";
+import CultifyTopNav from "./pages/dashboard/components/cultifyTopNav";
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Routes>
+          <Route path="*" Component={ErrorPage} />
           <Route path="/login" element={<Login />} />
           <Route path="/otp" element={<Otp />} />
-          <Route path="/registration" element={<RegistrationRouter />} />
+          <Route path="/registration" element={<InvestorRegistrationPage />} />
+          <Route path="/investor/dashboard" element={<InvestorDashboard />} />
           <Route
-            path="/registration/consumer"
-            element={<ConsumerRegistration />}
+            path="/investor/dashboard/produceDetails"
+            element={<FarmProjectDetails />}
           />
-          <Route path="/registration/farmer" element={<FarmerRegistration />} />
           <Route
-            path="/registration/transporter"
-            element={<TransporterRegistration />}
+            path="/admin/dashboard/produceUpload"
+            element={<FarmProjects />}
           />
-          <Route path="/consumer/dashboard" element={<ConsumerDashboard />} />
+          <Route
+            path="/super-admin/dashboard"
+            element={<SuperAdminDashboardOptions />}
+          />
+          <Route path="/super-admin/dashboard/admins" element={<Admins />} />
+          <Route path="/topNav" element={<CultifyTopNav />} />
         </Routes>
       </Router>
     </div>
--- src/pages/dashboard/components/otp.jsx
+++ src/pages/dashboard/components/otp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import '../styles/otp.css'
+import "../styles/otp.css";
+import CultifyTopNav from "../../dashboard/components/cultifyTopNav";
 
 const Otp = () => {
   const [otp, setOtp] = useState("");
@@ -21,23 +22,35 @@ const Otp = () => {
   };
 
   return (
-    <div className="otpContainer">
-      <h1>OTP Verification</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="otp">OTP:</label>
-          <input
-            type="text"
-            id="otp"
-            value={otp}
-            onChange={handleOtpChange}
-            maxLength={6}
-          />
+    <CultifyTopNav
+      content={
+        <div className="otpContainer">
+          <h6 className="text-center mb-3">
+            Verity your<span> OTP</span>
+          </h6>
+          <p className="mb-3">
+            {" "}
+            Please check your email for the one-time password(OTP) <br /> to
+            complete the verification process.
+          </p>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="otp">Enter your OTP</label>
+              <input
+                type="text"
+                id="otp"
+                value={otp}
+                onChange={handleOtpChange}
+                maxLength={6}
+              />
+            </div>
+            <a href="#">Resend OTP</a>
+            <p className="error">{error}</p>
+            <button type="submit">Verify</button>
+          </form>
         </div>
-        <p className="error">{error}</p>
-        <button type="submit">Verify</button>
-      </form>
-    </div>
+      }
+    />
   );
 };
 
