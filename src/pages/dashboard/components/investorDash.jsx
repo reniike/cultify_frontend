94f747b307505e12df5b90dc5d77d449500c572b
NwachukwuSam <nwachukwusamuel123@gmail.com>
2023-07-12 13:09:46 +0100

    dashboard another update

--- src/pages/dashboard/components/investorDash.jsx
+++ src/pages/dashboard/components/investorDash.jsx
@@ -8,7 +8,7 @@ const TopNavBar = () => {
 
      
   return (
-    <nav className="body overflow-hidden">
+    <nav className="body overflow-hidden bg-[]">
         <div className="top-nav bg-white shadow-md p-2 md:p-5 w-full mx-auto h-14 flex justify-between items-center top-0 z-50">
             <img src={cultifyLogo} className="logo h-10 w-30" alt="Logo" />
             <img src={dropDown} className="drop" alt="drop" />
@@ -26,19 +26,19 @@ const TopNavBar = () => {
             </div>
             
         <div className="right-nav pt-4 pr-10 top-15 right-20">
-                    <h3 className="dash-board font-bold text-green-500 text-4xl pl-10"> DashBoard</h3>
+                    <h3 className="dash-board font-bold text-green-500 text-4xl pl-10"> Dashboard</h3>
             <div className="upper-boxes">
                     <h3 className="welcome font-bold text-black-500 text-2xl pl-10 pt-6" > Welcome User </h3>
                 <div className="investors-details grid grid-cols-3 h-13 gap-x-20 mr-6 p-6">
-                    <div className="number border-1.1 bg-lime-200 w-80 h-40 rounded-xl font-bold text-black-600 text-lg pl-3">Total Number of<br/> Investments</div>
-                    <div className="amount border-1.1 bg-lime-200 w-80 rounded-xl font-bold text-black-600 text-lg pl-3">Total Amount <br/>Invested</div>
-                    <div className="payments border-1.1 bg-lime-200 w-80 rounded-xl font-bold text-black-600 text-lg pl-3">UpComing <br/>Payments</div>
+                    <div className="number border-1.1 bg-white w-80 h-40 rounded-xl font-bold text-black-600 text-lg pl-3">Total Number of<br/> Investments</div>
+                    <div className="amount border-1.1 bg-white w-80 rounded-xl font-bold text-black-600 text-lg pl-3">Total Amount <br/>Invested</div>
+                    <div className="payments border-1.1 bg-white w-80 rounded-xl font-bold text-black-600 text-lg pl-3">UpComing <br/>Payments</div>
                 </div>
             </div>
                     <h3 className="project font-bold text-black-600 text-2xl pl-10 pt-6"> Farm Projects</h3>
             <div className="product-details grid grid-cols-2 h-22 gap-x-14 ml p-8">
-                <div className="project-one bg-lime-200  rounded-xl h-96 pl-20 font-bold text-black-600 text-lg pl-4">The Maize Project</div>
-                <div className="project-two  bg-lime-200  rounded-xl h-96 pl-22 font-bold text-black-600 text-lg pl-4">The Yam Project</div>
+                <div className="project-one bg-white rounded-xl h-96 pl-20 font-bold text-black-600 text-lg pl-4">The Maize Project</div>
+                <div className="project-two  bg-white  rounded-xl h-96 pl-22 font-bold text-black-600 text-lg pl-4">The Yam Project</div>
     
             </div>
         </div>
