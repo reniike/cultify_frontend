7b1e05e87227adcfa937bcda7d4701c32bb4af64
Moyinoluwa Michael <ogunsmoyin.m@gmail.com>
2023-07-12 08:12:24 +0100

    registration integration

--- src/App.js
+++ src/App.js
@@ -3,18 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './pages/login/components/login';
 import Otp from './pages/dashboard/components/otp';
 import InvestorRegistrationPage from './pages/dashboard/components/investorRegistrationPage';
-import InvestorDashboard from './pages/dashboard/components/investorDashboard';
 import ErrorPage from './pages/dashboard/components/errorPage';
 import FarmProjectDetails from './pages/dashboard/components/farmProjectDetails';
 import FarmProjects from './pages/dashboard/components/farmProjects';
 import SuperAdminDashboardOptions from './pages/dashboard/components/superAdminDashboardOptions';
 import Admins from './pages/dashboard/components/admins';
-<<<<<<< HEAD
-import NavBar from './pages/homepage/components/navBar';
 import HeroSection from './pages/homepage/components/heroSection';
-=======
-import TopNavBar from './pages/dashboard/components/investorDash';
->>>>>>> 51e540cac27976dd0f2e4dc95596dc002fb6f933
+import InvestorDashboard from "./pages/dashboard/components/investorDashboard";
 import PaystackPayment from './pages/dashboard/components/paystackPayment';
 import PaymentTransfer from './pages/dashboard/components/paystackTransfer';
 import CultifyTopNav from "./pages/dashboard/components/cultifyTopNav";
@@ -25,7 +20,6 @@ function App() {
       <Routes>
           <Route path='*' Component={ErrorPage}/>
           <Route path='/home' element={<HeroSection/>}/>
-          <Route path='/navbar' element={<NavBar/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/otp" element={<Otp/>}/>
           <Route path="/registration" element={<InvestorRegistrationPage/>} />
@@ -34,7 +28,6 @@ function App() {
           <Route path="/admin/dashboard/produceUpload" element={<FarmProjects/>}/>
           <Route path="/super-admin/dashboard" element={<SuperAdminDashboardOptions/>}/>
           <Route path="/super-admin/dashboard/admins" element={<Admins/>}/>
-          <Route path="/topNavBar" element={<TopNavBar/>}/>
           <Route path="/paystackPayment" element={<PaystackPayment/>}/>
           <Route path="/paystackTransfer" element={<PaymentTransfer/>}/>
           <Route path="/topNav" element={<CultifyTopNav />} />
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-<<<<<<< HEAD
 import './index.css'
-=======
-import "./index.css";
->>>>>>> b0ed0bd94ffb239f79a470613bb99f1d08f828fd
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
